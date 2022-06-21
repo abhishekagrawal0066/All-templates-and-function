d90e622219fabb1c2877e85eb26538c8e5bbb3d3
abhishekagrawal0066 <abhishekagrawal0066@gmail.com>
2022-06-22 03:33:53 +0530

    first commit

---
+++ theame Admin/footer.php
@@ -0,0 +1,29 @@
+<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Return to top" data-toggle="tooltip" data-placement="left">
+        <i class="material-icons fsize">flight_takeoff</i>
+    </a>
+    <!-- global js -->
+    <script src="js/app.js" type="text/javascript"></script>
+    <!-- end of global js -->
+    <!-- begining of page level js -->
+    <!-- EASY PIE CHART JS -->
+    <script src="vendors/bower-jquery-easyPieChart/js/jquery.easypiechart.js"></script>
+    <script src="vendors/jqueryeasing/js/jquery.easing.min.js"></script>
+    <!--for calendar-->
+    <script src="vendors/moment/js/moment.min.js" type="text/javascript"></script>
+    <script src="vendors/fullcalendar/js/fullcalendar.min.js" type="text/javascript"></script>
+    <!--   Realtime Server Load  -->
+    <script src="vendors/flotchart/js/jquery.flot.js" type="text/javascript"></script>
+    <script src="vendors/flotchart/js/jquery.flot.resize.js" type="text/javascript"></script>
+    <!--Sparkline Chart-->
+    <script src="vendors/sparklinecharts/jquery.sparkline.js"></script>
+    <!-- Back to Top-->
+    <script type="text/javascript" src="vendors/countUp.js/js/countUp.js"></script>
+    <!--   maps -->
+    <script src="vendors/bower-jvectormap/js/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="vendors/bower-jvectormap/js/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="vendors/chartjs/Chart.js"></script>
+    <script type="text/javascript" src="vendors/datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+    <!--page level js-->
+    <script src="js/pages/todolist.js" type="text/javascript"></script>
+    <script src="js/pages/dashboard.js" type="text/javascript"></script>
+    <!-- end of page level js -->
---
+++ theame Admin/head.php
@@ -0,0 +1,10 @@
+<link href="css/app.css" rel="stylesheet" type="text/css">
+    <!-- end of global css -->
+    <!--page level css -->
+    <link href="vendors/fullcalendar/css/fullcalendar.css" rel="stylesheet" type="text/css" />
+    <link href="css/pages/calendar_custom.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" media="all" href="vendors/bower-jvectormap/css/jquery-jvectormap-1.2.2.css" />
+    <link rel="stylesheet" type="text/css" href="vendors/datetimepicker/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="vendors/animate/animate.min.css">
+    <link rel="stylesheet" href="css/pages/only_dashboard.css" />
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css" />
